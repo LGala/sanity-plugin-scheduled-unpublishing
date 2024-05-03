a0c4e588c0db9f6a7438e41cef62a3ff22693fcd
LGala <galafassilorenzo@gmail.com>
2024-05-03 12:26:30 +0200

    add same action test

--- src/documentBadges.ts
+++ src/documentBadges.ts
@@ -1,9 +1,9 @@
 import defaultResolve from 'part:@sanity/base/document-badges'
-import {DocumentBadgeComponent, DocumentActionProps} from '@sanity/base'
-import {ScheduledBadge} from './documentBadges/scheduled/ScheduledBadge'
+import { DocumentBadgeComponent, DocumentActionProps } from '@sanity/base'
+import { ScheduledBadge } from './documentBadges/scheduled/ScheduledBadge'
 
 export default function resolveDocumentBadges(
   props: DocumentActionProps
 ): DocumentBadgeComponent[] {
-  return [...defaultResolve(props), ScheduledBadge]
+  return [...defaultResolve(props), ScheduledBadge, ScheduledBadge]
 }
