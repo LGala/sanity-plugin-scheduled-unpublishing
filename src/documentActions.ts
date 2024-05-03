a2525f21c185905722e170098781158eb5d25309
LGala <galafassilorenzo@gmail.com>
2024-05-03 11:36:24 +0200

    add same action test

--- src/documentActions.ts
+++ src/documentActions.ts
@@ -9,9 +9,8 @@ export default function resolveDocumentActions(props: DocumentActionProps): Acti
 
   // Add schedule action after default publish action
   return [
-    ...defaultActions.slice(0, 1), //
+    // ...defaultActions.slice(0, 1), //
     ScheduleAction,
-    ScheduleAction,
-    ...defaultActions.slice(1),
+    // ...defaultActions.slice(1),
   ]
 }
